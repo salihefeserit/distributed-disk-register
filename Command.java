672db0d912be810ba7c45216baec003ecb5804de
alptrhn <151152317+alptrhn@users.noreply.github.com>
2025-12-27 21:53:52 +0300

    command alanı ve constructor eklendi

--- Command.java
+++ Command.java
@@ -2,6 +2,29 @@ public class Command {
     public enum Type {
         SET,
         GET,
-        UNKOWN
+        UNKNOWN
     }
+
+
+private final Type type;
+private final String id;
+private final String message;
+
+public Command(Type type, String id, String message) {
+    this.type = type;
+    this.id = id;
+    this.message = message;
+}
+
+public Type getType() {
+    return type;
+}
+
+public String getId() {
+    return id;
+}
+
+public String getMessage() {
+    return message;
+}
 }
