e660c438bf5aad2da12939887c7c39fc4a7d4dbc
alptrhn <151152317+alptrhn@users.noreply.github.com>
2025-12-28 22:58:35 +0300

    messge server son hali tamamlandı

--- MessageServer.java
+++ MessageServer.java
@@ -1,17 +1,20 @@
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 
 public class MessageServer {
 
     private static final int PORT = 12345;
     private static final String STORAGE_DIR = "messages";
+
+
+    private final ConcurrentHashMap<String, String> memoryMap =
+            new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Sunucu Başlatılıyor...");
@@ -63,5 +66,46 @@ public class MessageServer {
                 }
             
 
+    }
+
+    private void handleSet(Command cmd, PrintWriter out) {
+        memoryMap.put(cmd.getId(), cmd.getMessage());
+
+        File file = new File(STORAGE_DIR, cmd.getId() + ".msg");
+
+        try (BufferedWriter =
+                     new BufferedWriter(new FileWriter(file))) {
+
+            writer.write(cmd.getMessage());
+            out.println("OK");            
+        } catch (IOException e) {
+            out.println("ERROR");
+        }
+    }
+
+    private void handleGet(Command cmd, PrintWriter out) {
+
+        if (memoryMap.containsKey(cmd.getId())) {
+            out.println(memoryMap.get(cmd.getId()));
+            return;
+        }
+
+        File file = new File(STORAGE_DIR, cmd.getId() + ".msg");
+
+        if  (!file.exists()) {
+            out.println("NOT_FOUND");
+            return;
+        }
+
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader(file))){
+
+            String content = reader.lines()
+                                   .collect(Collectors.joining("\n"));
+            memoryMap.put(cmd.getId(), content);
+            out.println(content); 
+        } catch (IOException e) {
+            out.println("ERROR");
+        }
     }
 }
