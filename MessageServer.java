7bbd309e826403b1f916a2905f4ad36cd5922b29
alptrhn <151152317+alptrhn@users.noreply.github.com>
2025-12-28 17:13:47 +0300

    başlangıç yapısı eklendi

--- MessageServer.java
+++ MessageServer.java
@@ -2,8 +2,12 @@ public class MessageServer {
 
     private static final int PORT = 12345;
     private static final String STORAGE_DIR = "messages";
-    
+
     public static void main(String[] args) {
         System.out.println("Sunucu Başlatılıyor...");
+        new MessageServer().start();
+    }
+    public void start(){
+        //başlangıç kodu eklenecek
     }
 }
