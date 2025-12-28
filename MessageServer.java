c3b0a4267f32ebc114182d31f1b94182750fc5dd
alptrhn <151152317+alptrhn@users.noreply.github.com>
2025-12-28 17:01:03 +0300

    port ve storage eklendi

--- MessageServer.java
+++ MessageServer.java
@@ -1,4 +1,8 @@
 public class MessageServer {
+
+    private static final int PORT = 12345;
+    private static final String STORAGE_DIR = "messages";
+    
     public static void main(String[] args) {
         System.out.println("Sunucu Başlatılıyor...");
     }
